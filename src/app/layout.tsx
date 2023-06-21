34732a90399b9786bb1f61393af8de96f28bd39e
Lubomir Gernath <gernathlub@gmail.com>
2023-06-21 19:40:44 +0200

    feat: created base structure and added font awesome icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <main className="flex min-h-screen min-w-screen flex-col items-center justify-between bg-bright dark:bg-dark text-bright dark:text-dark">
-          {children}
+          <div className='px-4 sm:px-6 min-h-screen w-full sm:w-full md:w-10/12 lg:w-4/5 xl:w-3/4 max-w-7xl'>
+            {children}
+          </div>
         </main>
       </body>
     </html>
