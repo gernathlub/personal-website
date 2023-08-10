4ef9743aa16d582bfb260b347e4ddd4940687b13
Lubomir Gernath <gernathlub@gmail.com>
2023-08-10 20:09:49 +0200

    feat: added base prisma config and working test component

--- src/app/[locale]/test/server/page.tsx
+++ src/app/[locale]/test/server/page.tsx
@@ -1,10 +1,14 @@
 import { useTranslations } from 'next-intl'
+import TestPrismaList from '@/components/TestPrismaList'
+
 
 export default function TestClientTranslations() {
     const t = useTranslations('Test')
+
     return (
       <div className="text-red font-bold">
         {t('test_server')}
+        <TestPrismaList></TestPrismaList>
       </div>
     )
   }
