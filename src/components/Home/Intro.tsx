d904b83d83a41950ba8f654352d04cc14fa9dead
Lubomir Gernath <gernathlub@gmail.com>
2023-09-02 20:07:04 +0200

    feat: responsive timeline

--- src/components/Home/Intro.tsx
+++ src/components/Home/Intro.tsx
@@ -7,13 +7,13 @@ export default function Intro() {
   return (
     <ContentContainer>
       <div className="flex flex-col mb-52 mt-36 px-18 space-y-64 md:mt-48 md:px-22 xl:px-28 md:space-y-80">
-        <h1 className="font-bold leading-relaxed text-5xl whitespace-pre-line sm:text-6xl">
+        <h1 className="font-bold leading-relaxed text-5xl whitespace-pre-line sm:text-6xl sm:leading-relaxed">
           {t("Looking for a\nFullStack Web Developer?")}
         </h1>
-        <h3 className="font-medium leading-normal px-10 text-4xl whitespace-pre-line sm:text-5xl">
+        <h3 className="font-medium leading-normal px-10 text-4xl whitespace-pre-line sm:text-5xl sm:leading-normal">
           {t("Let`s See if You Just\nFound One!")}
         </h3>
-        <h2 className="font-medium leading-relaxed mt-48 text-4xl sm:text-5xl">
+        <h2 className="font-medium leading-normal mt-48 text-4xl sm:text-5xl sm:leading-normal">
           {t("My Name is")}
           <p className="font-semibold text-primary">Ľubomír Gernáth</p>
         </h2>
--- src/components/Home/Timeline/TimelineItem.tsx
+++ src/components/Home/Timeline/TimelineItem.tsx
@@ -19,17 +19,17 @@ export default function TimelineItem({
   title: string
 }) {
   return (
-    <div className="group/outer h-60 relative">
-      <div className="flex relative top-1/2 -translate-y-1/2 w-screen group-even/outer:right-0 group-odd/outer:left-0 group-odd/outer:-translate-x-1/2">
-        <div className="w-1/2 group-odd/outer:text-right  float-left group-odd/outer:pr-60 group-even/outer:pl-60">
-          <div className="mt-6 text-left w-fit group-even/outer:float-left group-odd/outer:float-right">
-              <h3 className="font-semibold text-8xl text-primary">{year}</h3>
-              <div className="ml-1 text-2xl w-full">{title}</div>
+    <div className="group/outer h-80 lg:h-60 relative">
+      <div className="flex relative top-1/2 right-0 -translate-y-1/2 w-screen lg:group-odd/outer:left-0 lg:group-odd/outer:-translate-x-1/2">
+        <div className="float-left pl-28 w-1/2 sm:pl-32 md:pl-60 lg:group-odd/outer:text-right lg:group-odd/outer:pl-0 lg:group-odd/outer:pr-60">
+          <div className="mt-14 text-left w-fit float-left lg:group-odd/outer:float-right">
+              <h3 className="font-semibold text-6xl text-primary sm:text-8xl">{year}</h3>
+              <div className="ml-1 text-xl w-40 sm:text-2xl sm:w-48">{title}</div>
           </div>
         </div>
       </div>
-      <div className="absolute bg-dark border-dark border-8 border-solid delay-100 duration-500 group/inner h-10 ml-1.5 rounded-full top-1/2 transition-all -translate-x-1/2 -translate-y-1/2 w-10 z-2 hover:bg-secondary hover:h-40 hover:w-40">
-        <div className="absolute bg-dark delay-300 duration-300 h-1 rounded-xl top-1/2 transition-all -translate-y-1/2 w-40 z-1 group-hover/inner:bg-transparent group-hover/inner:w-0 group-odd/outer:left-0 group-even/outer:left-full group-odd/outer:-translate-x-full group-odd/outer:group-hover/inner:left-0 group-even/outer:group-hover/inner:right-0" />
+      <div className="absolute bg-dark border-dark border-8 border-solid delay-100 duration-500 group/inner h-10 ml-1.5 rounded-full top-1/2 transition-all -translate-x-1/2 -translate-y-1/2 w-10 z-2 hover:bg-secondary hover:h-32 hover:w-32 sm:hover:h-40 sm:hover:w-40">
+        <div className="absolute bg-dark hidden delay-300 duration-300 h-1 left-full rounded-xl top-1/2 transition-all -translate-y-1/2 w-40 z-1 group-hover/inner:bg-transparent group-hover/inner:w-0 group-hover/inner:right-0 md:block lg:group-odd/outer:left-0 lg:group-odd/outer:-translate-x-full lg:group-odd/outer:group-hover/inner:left-0" />
         <a
           href={redirPath}
           className="duration-200 overflow-visible opacity-0 relative transition-all group-hover/inner:opacity-100"
