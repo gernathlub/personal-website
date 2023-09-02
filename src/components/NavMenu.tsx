0115aeb93a151b176e52e0f0becba41a6b4268a1
Lubomir Gernath <gernathlub@gmail.com>
2023-09-02 15:37:20 +0200

    feat: added empty project and tech pages

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -7,8 +7,8 @@ export default function NavMenu() {
   return (
     <div className="h-full w-fit ml-auto mr-auto relative flex-grow space-x-1">
       <NavLink title={t("Home")} route="/" />
-      <NavLink title={t("Projects")} route="#" />
-      <NavLink title={t("Tech")} route="#" />
+      <NavLink title={t("Projects")} route="/projects" />
+      <NavLink title={t("Tech")} route="/tech" />
       <NavLink title={t("About")} route="/about" />
     </div>
   );
