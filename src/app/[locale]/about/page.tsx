080a5ae2504d7da0c87a896c633d59b577e7329e
Lubomir Gernath <gernathlub@gmail.com>
2023-09-20 18:55:02 +0200

    feat: about page personal item component and added delimiter

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,106 +1,97 @@
 import ContentContainer from "@/components/ContentContainer";
 import Image from "next/image";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useTranslations } from "next-intl";
 import {
   faEarthEurope,
   faBriefcase,
   faCakeCandles,
   faGraduationCap,
+  faUser,
 } from "@fortawesome/free-solid-svg-icons";
-import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import { faEnvelope, faFilePdf } from "@fortawesome/free-regular-svg-icons";
+import PersonalInfoItem from "@/components/About/PersonalInfoItem";
+import Delimiter from "@/components/Delimiter";
+import { DelimiterTypeEnum } from "@/enums/DelimiterTypeEnum";
 
 export default function About() {
   const t = useTranslations("About");
 
   return (
-    <div className="w-full pb-32">
+    <div className="pb-48 w-full">
       <ContentContainer>
-        <h3 className="text-5xl leading-relaxed font-bold mt-40 mb-28">
+        <h1 className="mb-64 mt-36 px-18 md:mb-80 md:mt-48 md:px-22 xl:px-28">
           {t("Who am I?")}
-        </h3>
-        <div className="flex lg:flex-row flex-col justify-center lg:justify-normal items-center">
-          <div className="w-full h-auto relative -ml-10 overflow-hidden mx-auto">
+        </h1>
+
+        <div className="flex flex-col items-center justify-center mb-48 lg:flex-row lg:justify-normal">
+          <div className="h-auto overflow-hidden relative w-full lg:w-1/2">
             <Image
               src={"/profilePhoto.jpg"}
               alt="ProfilePhoto"
               width={600}
               height={400}
-              className="w-auto h-auto profile-mask"
+              className="h-auto max-w-sm mx-auto my-auto profile-mask"
             ></Image>
           </div>
 
-          <div className="mt-10 lg:mt-0">
-            <div className="text-center lg:text-left mb-10 flex flex-col space-y-1 font-medium">
-              <div>
-                <FontAwesomeIcon
-                  icon={faEarthEurope}
-                  className="text-lg mr-2 text-primary"
-                />
-                <a
-                  href="https://goo.gl/maps/VzkAGPUB3ei4Zgfb9"
-                  className="hover:text-primary"
-                >
-                  {t("Kosice, Slovakia")}
-                </a>
-              </div>
+          <div className="flex flex-col mt-10 lg:mt-0 w-full lg:w-1/2 text-center lg:text-left mb-10 space-y-3 my-auto">
+            <PersonalInfoItem icon={faUser}>Ľubomír Gernáth</PersonalInfoItem>
 
-              <div>
-                <FontAwesomeIcon
-                  icon={faEnvelope}
-                  className="text-lg mr-2 text-primary"
-                />
-                <a
-                  href="mailto:gernathlub@gmail.com"
-                  className="hover:text-primary"
-                >
-                  GernathLub@gmail.com
-                </a>
-              </div>
+            <PersonalInfoItem
+              icon={faEarthEurope}
+              redirPath="https://goo.gl/maps/VzkAGPUB3ei4Zgfb9"
+            >
+              {t("Kosice, Slovakia")}
+            </PersonalInfoItem>
 
-              <div>
-                <FontAwesomeIcon
-                  icon={faCakeCandles}
-                  className="text-lg mr-2 text-primary"
-                />
-                23. {t("January")} 1999
-              </div>
+            <PersonalInfoItem
+              redirPath="mailto:gernathlub@gmail.com"
+              icon={faEnvelope}
+            >
+              GernathLub@gmail.com
+            </PersonalInfoItem>
 
-              <div>
-                <FontAwesomeIcon
-                  icon={faGraduationCap}
-                  className="text-lg mr-2 text-primary"
-                />
-                {t("Master's Degree (Computer Science)")}
-              </div>
+            <PersonalInfoItem icon={faCakeCandles}>
+              23. {t("January")} 1999
+            </PersonalInfoItem>
 
-              <div>
-                <FontAwesomeIcon
-                  icon={faBriefcase}
-                  className="text-lg mr-2 text-primary"
-                />
-                <a href="https://esten.eu" className="hover:text-primary">
-                  Esten s.r.o.
-                </a>
-              </div>
-            </div>
+            <PersonalInfoItem icon={faGraduationCap}>
+              {t("Master's Degree (Computer Science)")}
+            </PersonalInfoItem>
 
-            <div className="text-left">
-              <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Accusamus repudiandae ab a recusandae odit odio mollitia error
-                vel neque, vero repellat debitis voluptatem rem maiores eaque,
-                eius architecto animi. Aliquid.
-              </p>
-              <p className="mt-5">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Accusamus repudiandae ab a recusandae odit odio mollitia error
-                vel neque, vero repellat debitis voluptatem rem maiores eaque,
-                eius architecto animi. Aliquid.
-              </p>
-            </div>
+            <PersonalInfoItem icon={faBriefcase} redirPath="https://esten.eu">
+              Esten s.r.o.
+            </PersonalInfoItem>
           </div>
         </div>
+      </ContentContainer>
+
+      <Delimiter type={DelimiterTypeEnum.DarkToDark} />
+
+      <ContentContainer>
+        <div className="mt-80">
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus
+            repudiandae ab a recusandae odit odio mollitia error vel neque, vero
+            repellat debitis voluptatem rem maiores eaque, eius architecto
+            animi. Aliquid.
+          </p>
+          <p className="mt-5">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus
+            repudiandae ab a recusandae odit odio mollitia error vel neque, vero
+            repellat debitis voluptatem rem maiores eaque, eius architecto
+            animi. Aliquid.
+          </p>
+        </div>
+        {/* <div className="text-4xl mt-40">
+        <FontAwesomeIcon
+                  icon={faFilePdf}
+                  className="mr-2 text-primary text-6xl -mb-3"
+                />
+                <a href="" className="hover:text-primary font-medium align-middle" download>
+                  Download CV
+                </a>
+        </div> */}
       </ContentContainer>
     </div>
   );
