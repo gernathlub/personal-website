8d56ee06871b5c465ec1d5aee04d45aec0100977
Lubomir Gernath <gernathlub@gmail.com>
2023-10-08 17:43:32 +0200

    feat: current subpage nav menu indication

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -2,10 +2,20 @@
 import Link from "next/link"
 import { IconDefinition } from "@fortawesome/fontawesome-svg-core"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { usePathname } from "next/navigation"
+import { useEffect, useState } from "react"
 
 export default function NavLink({title, route, icon}: {title: string, route: string, icon: IconDefinition}) {
+    const pathname = usePathname()
+    const [isActive, setActive] = useState<boolean>(false)
+
+    useEffect(() => {
+        setActive(route.split('/')[1] === pathname.split('/')[1])
+        console.log(`${route} ${isActive}`)
+    }, [pathname])
+
     return (
-        <Link href={route} scroll={false} className="border-b-4 border-solid border-transparent flex flex-row justify-center text-5xl lg:text-xl duration-250 ease-in font-semibold px-5 py-2 text-white transition lg:hover:border-secondary lg:hover:text-primary">
+        <Link href={route} scroll={false} className={`border-b-4 border-solid border-transparent duration-150 flex flex-row font-semibold justify-center text-5xl px-5 py-2 transition lg:text-xl lg:hover:border-secondary  ${ isActive ? 'text-primary' : 'text-white lg:hover:text-primary'}`}>
             <FontAwesomeIcon icon={icon} className="inline-block mr-4 lg:hidden"></FontAwesomeIcon>
             {title}
         </Link>
