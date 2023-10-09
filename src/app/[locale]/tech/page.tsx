4fb5e052533024a94c05ceaa66b65049cfb9200e
Lubomir Gernath <gernathlub@gmail.com>
2023-10-09 19:37:47 +0200

    chore: added translations

--- src/app/[locale]/tech/page.tsx
+++ src/app/[locale]/tech/page.tsx
@@ -3,24 +3,27 @@ import Delimiter from "@/components/Delimiter";
 import TechCategory from "@/components/Tech/TechCategory";
 import TechItem from "@/components/Tech/TechItem";
 import { DelimiterTypeEnum } from "@/enums/DelimiterTypeEnum";
+import { useTranslations } from "next-intl";
 
 export default function Tech() {
+  const t = useTranslations("Tech")
+
   return (
     <div className="flex flex-col flex-grow justify-center text-center">
-      <h1 className="mt-60 mb-60">Technologies</h1>
+      <h1 className="mt-60 mb-60">{ t("Technologies") }</h1>
         <Delimiter type={DelimiterTypeEnum.DarkToSecondary}/>
         <ContentContainer extraClass="bg-secondary">
             <div className="pb-32 pt-40 text-lg">
-                All following skills are evaluated based on self-evaluation, with following scale:
+                { t("eval_process") }:
                 <ul className="list-disc mt-4 mx-auto text-left w-full xl:w-1/2">
                     <li className="mb-3">
-                        <span className="font-semibold">Low Range</span> - Ability to use the technology (meaning its basic and the most commoly used features) with occasional need of help from docs. or internet.
+                        <span className="font-semibold">{ t("Low Range") }</span> - { t("low_range_descr") }
                     </li>
                     <li className="mb-3">
-                        <span className="font-semibold">Mid Range</span> - Knowing majority of basic features without need of docs.. Ability to use advanced features with occasional need to &quot;google&quot; a thing or two.
+                        <span className="font-semibold">{ t("Mid Range") }</span> - { t("mid_range_descr") }
                     </li>
                     <li className="mb-3">
-                        <span className="font-semibold">High Range</span> - Using majority of basic and advanced features without any problem. Deep knowledge of systems core and ability to adjust (or extend) the technology with help of the docs..
+                        <span className="font-semibold">{ t("High Range") }</span> - { t("high_range_descr") }
                     </li>
                 </ul>
             </div>
@@ -28,7 +31,7 @@ export default function Tech() {
         <Delimiter type={DelimiterTypeEnum.SecondaryToDark}/>
 
       <ContentContainer extraClass="mb-20 mt-40">
-        <TechCategory title="Programming and Markup Languages">
+        <TechCategory title={ t("Programming and Markup Languages") }>
           <TechItem title="Python" progress={ 80 } />
           <TechItem title="TypeScript" progress={ 70 } />
           <TechItem title="C#" progress={ 50 } />
@@ -37,7 +40,7 @@ export default function Tech() {
           <TechItem title="JavaScript" progress={ 60 } />
         </TechCategory>
 
-        <TechCategory title="Frameworks and Libraries">
+        <TechCategory title={ t("Frameworks and Libraries") }>
             <TechItem title="Django" progress={ 90 } />
             <TechItem title="Angular" progress={ 45 } />
             <TechItem title="React.js" progress={ 60 } />
@@ -47,9 +50,11 @@ export default function Tech() {
             <TechItem title="HTMX" progress={ 80 } />
             <TechItem title="TailwindCSS" progress={ 75 } />
             <TechItem title="Bootstrap" progress={ 50 } />
+            <TechItem title="WPF" progress={ 25 } />
+            <TechItem title="Prisma" progress={ 40 } />
         </TechCategory>
 
-        <TechCategory title="Databases">
+        <TechCategory title={ t("Databases") }>
             <TechItem title="PostgreSQL" progress={ 70 } />
             <TechItem title="MongoDB" progress={ 50 } />
             <TechItem title="SQL Server" progress={ 40 } />
@@ -57,7 +62,7 @@ export default function Tech() {
             <TechItem title="CosmosDB" progress={ 30 } />
         </TechCategory>
 
-        <TechCategory title="Development and Design Tools">
+        <TechCategory title={ t("Development and Design Tools")}>
             <TechItem title="Git" progress={ 80 } />
             <TechItem title="Figma" progress={ 50 } />
             <TechItem title="Docker" progress={ 70 } />
@@ -65,10 +70,12 @@ export default function Tech() {
             <TechItem title="Jira" progress={ 50 } />
         </TechCategory>
 
-        <TechCategory title="Cloud and DevOps">
+        <TechCategory title={ t("Cloud and DevOps") }>
             <TechItem title="AWS" progress={ 50 } />
             <TechItem title="Serverless" progress={ 40 } />
             <TechItem title="Azure" progress={ 30 } />
+            <TechItem title="PowerAutomate" progress={25} />
+            <TechItem title="Vercel" progress={ 20 } />
         </TechCategory>
       </ContentContainer>
     </div>
