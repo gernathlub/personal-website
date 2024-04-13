a542aab16715b0ab0764e59aae7170ab637d11d1
Lubomir Gernath <gernathlub@gmail.com>
2024-04-13 16:32:59 +0200

    feat: updated global metadata

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -14,6 +14,15 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Lubomir Gernath - Experienced Fullstack Software Developer in Kosice, Slovakia | Web Development Expert in Python, TypeScript, and C#",
   description: "Unlock the full potential of your digital projects with a seasoned fullstack software developer based in Kosice, Slovakia. With years of hands-on experience, I specialize in web development using Python, TypeScript, and C#. Elevate your online presence and streamline your processes with my expertise in crafting robust and scalable solutions. Let's collaborate to bring your ideas to life and create cutting-edge web applications tailored to your unique needs.",
+  keywords: ['Fullstack', 'Kosice', 'programmer', 'contractor', 'python', 'dotnet', '.net', 'angular', 'react'],
+  creator: 'Lubomir Gernath',
+  alternates: {
+    canonical: '/',
+    languages: {
+      'en-US': '/',
+      'sk-SK': '/sk',
+    },
+  },
 };
 
 export default async function LocaleLayout({
