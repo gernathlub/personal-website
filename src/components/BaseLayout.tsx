3f2f2f0c3d3ad84e217bf33049b2a5808cb03dab
Lubomir Gernath <gernathlub@gmail.com>
2024-04-21 17:32:06 +0200

    feat: use of base font

--- src/components/BaseLayout.tsx
+++ src/components/BaseLayout.tsx
@@ -1,15 +1,12 @@
 'use client'
-import { Jura } from 'next/font/google'
 import Footer from '@/components/Footer'
 import Header from '@/components/Header'
-
-const font = Jura({ subsets: ['latin'] })
 
 export default function BaseLayout({
     children,
 }: Readonly<{ children: React.ReactNode }>) {
     return (
-        <body className={font.className}>
+        <body>
             <main className="min-w-screen flex min-h-screen flex-col overflow-x-hidden bg-dark text-dark">
                 <Header></Header>
                 {children}
