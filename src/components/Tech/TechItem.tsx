616d042c083ccfb75d9f382c01760bbb31ba1c58
Lubomir Gernath <gernath@esten.sk>
2025-08-10 15:49:32 +0200

    feat: data update

--- src/components/Tech/TechItem.tsx
+++ src/components/Tech/TechItem.tsx
@@ -1,13 +1,36 @@
+import Image from 'next/image'
+
+export type Certificate = {
+    logoPath: string
+    link?: string
+}
+
 export default function TechItem({
     title,
     progress,
+    certificate = null,
 }: Readonly<{
     title: string
     progress: number
+    certificate?: Certificate | null
 }>) {
     return (
         <div className="mx-5 flex w-64 flex-col p-4 text-left text-xl">
-            <p className="ml-1">{title}</p>
+            <span className="flex flex-row space-x-1">
+                <p className="ml-1">{title}</p>
+                {certificate ? (
+                    <a href={certificate.link ?? '#'} target="_blank">
+                        <Image
+                            width={30}
+                            height={30}
+                            src={certificate.logoPath}
+                            alt="Cert. logo"
+                        />
+                    </a>
+                ) : (
+                    ''
+                )}
+            </span>
             <div className="mt-1 h-3 w-full rounded-full bg-secondary">
                 <span
                     className="block h-full rounded-full bg-primary"
